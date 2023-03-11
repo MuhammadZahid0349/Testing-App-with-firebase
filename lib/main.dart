37b3eb967d79226c1fb791d4adccf465b3c701f5
MuhammadZahid0349 <zahidhashim1212@gmail.com>
2023-03-11 23:51:17 +0500

    --- Fetch data ---

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +13,29 @@ import 'package:testing_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  ///// for all collection data
+  // QuerySnapshot snapshot =
+  //     await FirebaseFirestore.instance.collection("users").get();
+  // for (var doc in snapshot.docs) {
+  //   print(doc.data().toString());
+  // }
+
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection("users")
+  //     .doc("Yw1th07oKjeMsO8EdOQg")
+  //     .get();
+  // print(snapshot.data());
+
+  Map<String, dynamic> newUser = {
+    "name": "Faizan",
+    "email": "Faizan@gmail.com",
+  };
+  await firestore.collection("users").add(newUser);
+  print("New user saved");
+
   runApp(const MyApp());
 }
 
